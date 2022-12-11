fc867d7900623a4140138fa4c6a4b6a1d8e2b29a
dino <mypascal2000@gmail.com>
2022-12-11 11:01:46 -0800

    Fixing issue where changing remote token.secret would not stop user from continuing using same old token.secret generated JWT.

--- src/main/java/io/dino/learning/apigateway/AuthorizationHeaderFilter.java
+++ src/main/java/io/dino/learning/apigateway/AuthorizationHeaderFilter.java
@@ -4,7 +4,6 @@ import io.jsonwebtoken.Jwts;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
